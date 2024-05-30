2ca1f07f344f1763a2aa71658f2caaefcadbbf9d
Hriday Paras Bhagtani <96878347+Hridxyz@users.noreply.github.com>
2024-05-30 16:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (57.55%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,9 +8,16 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
+        if (node == null || node.next == null) {
+            throw new IllegalArgumentException("Invalid node to delete.");
+        }
+
         // Copy the value from the next node to the current node
         node.val = node.next.val;
-        // Link the current node to the node after the next node, effectively deleting the next node
+
+        // Bypass the next node
         node.next = node.next.next;
     }
+
+    
 }
