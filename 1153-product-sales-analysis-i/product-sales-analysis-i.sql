c14558218e60e4a0c1e697b4ee93648cb5086103
Hriday Paras Bhagtani <96878347+Hridxyz@users.noreply.github.com>
2024-06-06 12:00:43 +0530

    Time: 1047 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT b.product_name, a.year, a.price
+FROM Sales AS a
+RIGHT JOIN Product AS b
+ON a.product_id = b.product_id 
+WHERE a.year IS NOT NULL
+ORDER BY b.product_name
