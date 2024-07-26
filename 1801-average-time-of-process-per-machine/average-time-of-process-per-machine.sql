bf132fe6a070a11ce516423b541b501990016f17
Hriday Paras Bhagtani <96878347+Hridxyz@users.noreply.github.com>
2024-07-26 22:45:10 +0530

    Time: 204 ms (76.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,13 @@
+SELECT 
+    machine_id,
+    ROUND(AVG(processing_time), 3) AS processing_time
+FROM (
+    SELECT 
+        machine_id, 
+        process_id, 
+        MAX(CASE WHEN activity_type = 'end' THEN timestamp ELSE NULL END) - 
+        MAX(CASE WHEN activity_type = 'start' THEN timestamp ELSE NULL END) AS processing_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS ProcessTimes
+GROUP BY machine_id;
